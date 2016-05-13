c2f0463f617702f82d3aeba7516ef9dc203bedcd
kickscar <kickscar@gmail.com>
2016-05-13 13:38:12 +0900

    final

--- src/servlet/HelloWorldServlet.java
+++ src/servlet/HelloWorldServlet.java
@@ -9,10 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/a/hello")
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost( request, response );	
 	}
